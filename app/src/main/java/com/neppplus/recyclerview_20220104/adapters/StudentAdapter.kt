bbf43c0f74f5e23d38ca92446d63c0b9bebbcb9b
nepp-plus <nepp.plus@gmail.com>
2022-01-04 11:32:46 +0900

    MyViewHolder를 -> 어댑터가 다룰 자료형으로 설정해줌.

--- app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 // 학생 한명 데이터 + 학생 한명 xml 조합 => 위치에 맞게 보내주는 역할.
 // 안드로이드에서 기초 작업을 제공. => 상속을 받아서 일부 수정.
 
-class StudentAdapter : RecyclerView.Adapter<>() {
+class StudentAdapter : RecyclerView.Adapter<StudentAdapter.MyViewHolder>() {
 
 //    StudentAdapter 클래스 안에서, 내부 전용 클래스 추가 생성.
     inner class MyViewHolder(row: View) : RecyclerView.ViewHolder(row) {
