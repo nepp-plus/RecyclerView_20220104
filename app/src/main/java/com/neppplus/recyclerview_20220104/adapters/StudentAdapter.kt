3a5ff2120608f1de559af02f5528c7a8450cd25b
nepp-plus <nepp.plus@gmail.com>
2022-01-04 12:18:17 +0900

    클래스의 메쏘드 추가 -> 나이 계산 기능을 받아서 활용

--- app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
@@ -35,7 +35,7 @@ class StudentAdapter(
             txtName.text = data.name
             txtAddress.text = data.address
 
-            txtAge.text = "(${data.birthYear}세)"
+            txtAge.text = "(${data.getAge()}세)"
 
         }
 
